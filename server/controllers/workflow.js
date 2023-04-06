806bf511a6f60fe1537a4dbb1e13d922aae92802
tzutingspace <tzutingh2@gmail.com>
2023-04-06 14:59:19 +0800

    [Add] API for get workflow Info by ID

--- server/controllers/workflow.js
+++ server/controllers/workflow.js
@@ -1,10 +1,16 @@
 import * as Workflow from '../models/workflow.js';
+import { vaildInterger } from '../utils/utli.js';
+import { StatusCodes } from 'http-status-codes';
+import { CustomError } from '../utils/customError.js';
 
-export const getWorkflow = async (req, res) => {
-  const { id } = req.params;
+export const getWorkflow = async (req, res, next) => {
   console.log('@controller getWorkflow');
-  const wf = await Workflow.getWorkflowById(id);
-  return res.json({ msg: wf });
+  const { id } = req.params;
+  if (!vaildInterger(id)) {
+    return next(new CustomError('Query Params Error', StatusCodes.BAD_REQUEST));
+  }
+  const workflow = await Workflow.getWorkflowById(id);
+  return res.json({ msg: workflow });
 };
 
 export const createWorkflow = async (req, res) => {
