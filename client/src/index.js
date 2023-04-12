3b48a95d5cb4b17066627c64145d5a0a354a744a
tzutingspace <tzutingh2@gmail.com>
2023-04-12 13:44:03 +0800

    [Init] Init client project

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Build from './pages/Build';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Build />
+  </React.StrictMode>
+);
