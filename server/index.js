ff25fdb463fe5a4a32b712563c9cabcac2d54e40
tzutingspace <tzutingh2@gmail.com>
2023-04-21 21:59:54 +0800

    [Add] socket for trigger finished

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express, { json } from 'express';
-import dotenv from 'dotenv';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
+import dotenv from 'dotenv';
 import StatusCodes from 'http-status-codes';
 import cors from 'cors';
+
+//
 import CustomError from './utils/customError.js';
+
+// ROUTER
 import { router as workflow } from './routers/workflow.js';
 import { router as funcitons } from './routers/tool.js';
 import { router as trigger } from './routers/trigger.js';
@@ -12,6 +18,15 @@ import { router as admin } from './routers/admin.js';
 dotenv.config();
 
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 
@@ -27,6 +42,26 @@ app.use('/admin', admin);
 app.use('/api/', workflow);
 app.use('/api/', funcitons);
 app.use('/api/', trigger);
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('trigger', (roomName) => {
+    socket.join(roomName);
+    console.log(`User joined room: ${roomName}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnect: ${socket.id}`);
+  });
+});
+
+// trigger success
+app.post('/triggerFinish', (req, res) => {
+  const { roomId, data } = req.body;
+  io.to(roomId).emit('triggerFinish', { message: data });
+  res.send({ data: 'accept' });
+});
 
 // Not Found
 app.use((req, res, next) => {
@@ -47,6 +82,6 @@ app.use((err, req, res, next) => {
     .json({ message: customError.message, status: customError.statusCode });
 });
 
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log(`The application is running on on port ${PORT}`)
 );
