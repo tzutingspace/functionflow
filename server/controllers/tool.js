842f985a089e600f8db8c7d6a1ab4d0c56879b4c
tzutingspace <tzutingh2@gmail.com>
2023-05-10 23:51:21 +0800

    [Modify] rename variable name(requirement) @server

--- server/controllers/tool.js
+++ server/controllers/tool.js
@@ -11,26 +11,20 @@ export const getTriggers = async (req, res) => {
 export const searchTrigger = async (req, res) => {
   console.log('@controller searchTrigger');
   const id = req.params.id === undefined ? 1 : Number(req.params.id);
-  const requriement = { id };
-  const tool = await DBTool.getTriggers(requriement);
+  const tool = await DBTool.getTriggers({ id });
   return res.json({ data: tool });
 };
 
 export const getTools = async (req, res) => {
   console.log('@controller getTools');
-  console.log(req.params);
   const type = req.params.type === undefined ? 'all' : req.params.type;
-  const requriement = type === 'all' ? {} : { type };
-  const tools = await DBTool.getTools(requriement);
+  const tools = await DBTool.getTools(type === 'all' ? {} : { type });
   return res.json({ data: tools });
 };
 
 export const searchTool = async (req, res) => {
   console.log('@controller searchTool');
   const id = req.params.id === undefined ? 1 : Number(req.params.id);
-  const requriement = { id };
-  const tool = await DBTool.getTools(requriement);
+  const tool = await DBTool.getTools({ id });
   return res.json({ data: tool });
 };
-
-// export { getTriggers, searchTrigger, getTools, searchTool };
--- server/models/tool.js
+++ server/models/tool.js
@@ -1,34 +1,34 @@
 import pool from '../utils/db.js';
 
-async function getTriggers(requriement = {}) {
-  console.log('@ models getTriggers', requriement);
+async function getTriggers(requirement = {}) {
+  console.log('@ models getTriggers', requirement);
   const condition = { sql: '', binding: [] };
-  if (requriement.id != null) {
+  if (requirement.id != null) {
     condition.sql = 'WHERE id = ?';
-    condition.binding = [requriement.id];
-  } else if (requriement.type != null) {
+    condition.binding = [requirement.id];
+  } else if (requirement.type != null) {
     condition.sql = 'WHERE type = ?';
-    condition.binding = [requriement.type];
-  } else if (requriement.keyword != null) {
+    condition.binding = [requirement.type];
+  } else if (requirement.keyword != null) {
     condition.sql = 'WHERE external_name LIKE ?';
-    condition.binding = [`%${requriement.keyword}%`];
+    condition.binding = [`%${requirement.keyword}%`];
   }
   const triggersQuery = `SELECT * FROM \`trigger_functions\`${condition.sql} ORDER BY id`;
   const [rows] = await pool.query(triggersQuery, condition.binding);
   return rows;
 }
 
-async function getTools(requriement = {}) {
+async function getTools(requirement = {}) {
   const condition = { sql: '', binding: [] };
-  if (requriement.id != null) {
+  if (requirement.id != null) {
     condition.sql = 'WHERE id = ?';
-    condition.binding = [requriement.id];
-  } else if (requriement.type != null) {
+    condition.binding = [requirement.id];
+  } else if (requirement.type != null) {
     condition.sql = 'WHERE type = ?';
-    condition.binding = [requriement.type];
-  } else if (requriement.keyword != null) {
+    condition.binding = [requirement.type];
+  } else if (requirement.keyword != null) {
     condition.sql = 'WHERE title LIKE ?';
-    condition.binding = [`%${requriement.keyword}%`];
+    condition.binding = [`%${requirement.keyword}%`];
   }
   const toolsQuery = `SELECT * FROM \`functions\`${condition.sql} ORDER BY type`;
   const [rows] = await pool.query(toolsQuery, condition.binding);
