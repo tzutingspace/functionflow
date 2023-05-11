ec769db0f822ea9ed3e5aa7d240147d2e1087abf
tzutingspace <tzutingh2@gmail.com>
2023-05-11 12:54:08 +0800

    [Modify] saltRounds only use in hashPassword functiion @server

--- server/utils/utli.js
+++ server/utils/utli.js
@@ -2,6 +2,8 @@ import moment from 'moment-timezone';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+import { userProvider } from '../config/userProvider.js';
+
 // convert to UTC
 // 預設目前進來的時間都是Asia/Taipei
 export function convertLocalToUTC(localTime, timezone = 'Asia/Taipei') {
@@ -23,45 +25,38 @@ export function getNowTime() {
 export function calculateTime(inputTime, internalSeconds) {
   const interval = moment.duration(parseInt(internalSeconds, 10), 'seconds');
   const executeTime = moment(inputTime);
-  const nextTime = executeTime
-    .clone()
-    .add(interval)
-    .format('YYYY-MM-DD HH:mm:ss');
-  return nextTime;
+  return executeTime.clone().add(interval).format('YYYY-MM-DD HH:mm:ss');
 }
 
 // 確認為正整數(0-9)
-export function vaildInterger(sting) {
+export function validInteger(sting) {
   const regexNumber = /^[0-9]*$/;
   return regexNumber.test(sting);
 }
 
-export function ValidateEmail(inputText) {
+export function validateUserProvider(inputProvide) {
+  return userProvider.includes(inputProvide);
+}
+
+export function validateEmail(inputText) {
   // reference: https://www.w3schools.com/jsref/jsref_obj_regexp.asp
   // reference: https://www.geeksforgeeks.org/form-validation-using-html-javascript/
   const mailFormat = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/g;
-  if (mailFormat.test(inputText)) {
-    return true;
-  }
-  return false;
+  return mailFormat.test(inputText);
 }
 
-export function ValidatePassword(inputText) {
+export function validatePassword(inputText) {
   // reference: https://stackoverflow.com/questions/12090077/javascript-regular-expression-password-validation-having-special-characters
   // const pwdFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-  // 用可顯示字元
+  // change to printable characters.
   const pwdFormat = /^[\x20-\x7E]{6,16}$/;
-  if (pwdFormat.test(inputText)) {
-    return true;
-  }
-  return false;
+  return pwdFormat.test(inputText);
 }
-
-// 處理密碼 reference: https://www.makeuseof.com/nodejs-bcrypt-hash-verify-salt-password/
-const saltRounds = 10;
 
 // 產生 hashPassword
 export async function hashPassword(plaintextPassword) {
+  // reference: https://www.makeuseof.com/nodejs-bcrypt-hash-verify-salt-password/
+  const saltRounds = 10;
   return bcrypt.hash(plaintextPassword, saltRounds);
 }
 
