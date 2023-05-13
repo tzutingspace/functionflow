8618612288a9a0112b023ca58288594326fa7f7b
tzutingspace <tzutingh2@gmail.com>
2023-05-13 16:37:42 +0800

    [Modify] job controller @server

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -1,19 +1,13 @@
-import * as DBWorkflow from '../models/workflow.js';
+import * as DBJob from '../models/job.js';
 import { validInteger } from '../utils/utli.js';
 
 import BadRequestError from '../utils/errors/badRequestError.js';
 
 // CREATE JOB
-export const createJob = async (req, res, next) => {
-  console.debug('@controller createJob');
-  console.debug('request Body', req.body);
-  const { workflowInfo, jobsInfo } = req.body;
-  const insertJobSeq = jobsInfo.sequence;
-  const workflowId = workflowInfo.id;
-
-  if (!validInteger(insertJobSeq)) {
-    return next(new BadRequestError('Query Params Error'));
-  }
+export const createJob = async (req, res) => {
+  console.debug('@controller createJob', req.body);
+  const { jobsInfo } = req.body;
+  const workflowId = req.workflowDatabaseResult.id;
 
   const necessaryInfo = {
     name: jobsInfo.job_name,
@@ -22,20 +16,18 @@ export const createJob = async (req, res, next) => {
     customer_input: JSON.stringify(jobsInfo.customer_input),
   };
 
-  const result = await DBWorkflow.createJob(workflowId, necessaryInfo);
+  const result = await DBJob.createJob(workflowId, necessaryInfo);
 
   return res.json({ data: result });
 };
 
 // UPDATE JOB
 export const updateJob = async (req, res, next) => {
-  console.log('@controller updateJob');
-  console.log('request Body', req.body);
+  console.log('@controller updateJob', req.body);
   const { jobsInfo } = req.body;
-  const updateJobSeq = jobsInfo.sequence;
   const jobId = req.params.id;
 
-  if (!validInteger(jobId) || !validInteger(updateJobSeq)) {
+  if (!validInteger(jobId)) {
     return next(new BadRequestError('Query Params Error'));
   }
 
@@ -46,6 +38,7 @@ export const updateJob = async (req, res, next) => {
     customer_input: JSON.stringify(jobsInfo.customer_input),
   };
 
-  const result = await DBWorkflow.updateJob(jobId, necessaryInfo);
+  const result = await DBJob.updateJob(jobId, necessaryInfo);
+
   return res.json({ data: result });
 };
